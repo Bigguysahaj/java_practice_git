144a300828758cddf96e58dda8251607da1e8aaa
bigguysahaj <singhsahaj2001@gmail.com>
2022-08-04 17:44:34 +0530

    complete fibonaci

--- fibo.java
+++ fibo.java
@@ -1,3 +1,16 @@
-public class Fibonaci{
-    public static void main()
+
+public class fibo{
+    public static void main(String[] args){
+        System.out.println(fibo(10));
+    }
+
+    static int fibo(int n){
+
+        // if n is 0 or 1, return n
+        if ( n < 2 ) {
+            return n;
+        }
+
+        return fibo(n-1) + fibo(n-2);
+    }
 }
